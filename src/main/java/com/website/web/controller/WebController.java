d00955f5aa541f19999001a05c55bcb0397ffad7
leihan <447882092@qq.com>
2023-07-12 22:04:29 +0800

    初始提交

---
+++ src/main/java/com/website/web/controller/WebController.java
@@ -0,0 +1,12 @@
+package com.website.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@CrossOrigin(origins = "*")
+public class WebController {
+
+
+}
