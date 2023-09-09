f074fce1d49701b5ccea695cf2ea2f33df20b87a
leihan <447882092@qq.com>
2023-09-09 20:47:16 +0800

    Test

--- src/main/java/com/website/web/WebApplication.java
+++ src/main/java/com/website/web/WebApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WebApplication {
 
+    //Delete this comment (Testing comment)
     public static void main(String[] args) {
         SpringApplication.run(WebApplication.class, args);
     }
